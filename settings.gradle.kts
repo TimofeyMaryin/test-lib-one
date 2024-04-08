084909a1d76e915b04857bf169c00eb4b3b3178e
Timofey Maryin <nemotim4ic@gmail.com>
2024-04-08 17:50:56 +0500

    create project

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TestLib3"
+include(":app")
+include(":test-lib-three")
---
+++ test-lib-three/src/main/java/test/android/test_lib_three/TestLib.kt
@@ -0,0 +1,13 @@
+package test.android.test_lib_three
+
+object TestLib {
+
+    fun funcOne(): String {
+        return "One"
+    }
+
+    fun funcTwo(): String {
+        return "Two"
+    }
+
+}
